01bd5495ba792a484e28604dfbf1e7721147149d
SagarJm <sagarjm1111@gmail.com>
2026-01-11 12:16:27 +0530

    Dart Collections:

--- lib/INTERFACE.dart
+++ lib/INTERFACE.dart
@@ -1,5 +1,37 @@
 void main(){
-  print('Dart Lambda Expression ');
+  print('Lexical Closures ');
+
+
+  // Definition 1
+  // A closures is a  function   that has  access to the parent scope , even after the scope has closed
+
+  String message="Dart is god";
+
+  Function showMessage=(){
+    message = "Dart is  awesome";
+    print(message);
+  };
+
+  showMessage();
+
+  // Definition 2
+  // A closures is a function object that has access to variable in its lexical scope ,
+  // even when the function is used outside of its original scope
+
+  Function talk =(){
+    String msg='Hi';
+    Function say= (){
+      msg="Hello";
+      print(msg);
+    };
+    return say;
+  };
+
+  var speak= talk();
+
+  speak();
+
+
 
 
 
